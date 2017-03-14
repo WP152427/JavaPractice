e25e26e91aa600484a9a679e2b53106b4037d360
정유정 <@DESKTOP-MC74I78>
2017-03-14 15:51:42 +0900

    실습과제 2 제출

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -8,7 +8,7 @@ package org.dimigo.basic;
  * org.dimigo.basic
  *   |_ Operator
  *
- * 1. 개요 :
+ * 1. 개요 : 디미베네 연간 인건비
  * 2. 작성일 : 2017. 3. 14.
  * </pre>
  *
@@ -22,7 +22,16 @@ public class Operator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		long num=3;
+	    long gub=1700000;
+	    long store=1500;
+	    long ingwon = gub*12*num*store;
+	    
+	    System.out.println("<< 디미베네 연간 인건비 >>");
+	    System.out.println("월 평균 급여 : " + String.format("%,d", gub)  + "원");
+	    System.out.println("점포 내 직원 수 : " + num + "명");
+	    System.out.println("점포 수 : " + String.format("%,d", store) + "개\n");
+	    System.out.println("연간 인건비 : " + String.format("%,d", ingwon) + "원");
 
 	}
 
